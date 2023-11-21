fbe5ae28c6355148f5872ff8cc7b4d7e1ab64755
Sihomara Ochoa <sandy.ochoa.27@unsch.edu.pe>
2023-11-21 13:31:46 -0500

    SEcond First

---
+++ 02-base/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
+    AppRoutingModule,
+
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
